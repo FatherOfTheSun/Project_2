966c30496fd6827d0a08a89a062e975a3db93a83
alex musa <alex.musa1988@gmail.com>
2019-09-21 13:56:38 -0500

    create basic api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
-// var models = require("../models");
-// var inventory = models.inventory;
+var models = require("../models");
+var inventory = models.inventory;
 
 // Routes
 // =============================================================
@@ -11,79 +11,79 @@ module.exports = function(app) {
     res.json({ status: "OK" });
   });
 
-  // app.get("/api/all", function(req, res) {
-  //   inventory.findAll({}).then(function(results) {
-  //     res.json(results);
-  //   });
-  // });
+  app.get("/api/inventory", function(req, res) {
+    inventory.findAll({}).then(function(results) {
+      res.json(results);
+    });
+  });
 
-  // // Get a specific inventory
-  // app.get("/api/:item", function(req, res) {
-  //   inventory
-  //     .findAll({
-  //       where: {
-  //         item: req.params.item
-  //       }
-  //     })
-  //     .then(function(results) {
-  //       res.json(results);
-  //     });
-  // });
+  // Get a specific inventory
+  app.get("/api/:item", function(req, res) {
+    inventory
+      .findAll({
+        where: {
+          item: req.params.item
+        }
+      })
+      .then(function(results) {
+        res.json(results);
+      });
+  });
 
-  // // Get all inventorys of a specific category
-  // app.get("/api/category/:category", function(req, res) {
-  //   inventory
-  //     .findAll({
-  //       where: {
-  //         category: req.params.category
-  //       }
-  //     })
-  //     .then(function(results) {
-  //       res.json(results);
-  //     });
-  // });
+  // Get all inventorys of a specific category
+  app.get("/api/category/:category", function(req, res) {
+    inventory
+      .findAll({
+        where: {
+          category: req.params.category
+        }
+      })
+      .then(function(results) {
+        res.json(results);
+      });
+  });
 
-  // // Get all inventorys from a specific amount
-  // app.get("/api/amount/:amount", function(req, res) {
-  //   inventory
-  //     .findAll({
-  //       where: {
-  //         amount: req.params.amount
-  //       }
-  //     })
-  //     .then(function(results) {
-  //       res.json(results);
-  //     });
-  // });
+  // Get all inventorys from a specific amount
+  app.get("/api/amount/:amount", function(req, res) {
+    inventory
+      .findAll({
+        where: {
+          amount: req.params.amount
+        }
+      })
+      .then(function(results) {
+        res.json(results);
+      });
+  });
 
-  // // Add a inventory
-  // app.post("/api/new", function(req, res) {
-  //   console.log("inventory Data:");
-  //   console.log(req.body);
-  //   inventory
-  //     .create({
-  //       item: req.body.item,
-  //       amount: req.body.amount,
-  //       measure: req.body.measure,
-  //       category: req.body.category
-  //     })
-  //     .then(function(results) {
-  //       res.json(results);
-  //     });
-  // });
+  // Add a inventory
+  app.post("/api/inventory", function(req, res) {
+    // console.log("inventory Data:");
+    // console.log(req.body);
+    inventory
+      .create({
+        item: req.body.item,
+        amount: req.body.amount,
+        measure: req.body.measure,
+        category: req.body.category
+      })
+      .then(function(result) {
+        res.status(201).json(result);
+      });
+  });
 
-  // // Delete a inventory
-  // app.delete("/api/inventory/:id", function(req, res) {
-  //   console.log("inventory ID:");
-  //   console.log(req.params.id);
-  //   inventory
-  //     .destroy({
-  //       where: {
-  //         id: req.params.id
-  //       }
-  //     })
-  //     .then(function() {
-  //       res.end();
-  //     });
-  // });
+  // Delete a inventory
+  app.delete("/api/inventory/:id", function(req, res) {
+    console.log("inventory ID:");
+    console.log(req.params.id);
+    inventory
+      .destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function() {
+        res.end();
+      });
+  });
 };
