7aebc9666340f48c5306ad1230f269321f0ed26b
alex musa <alex.musa1988@gmail.com>
2019-09-21 22:00:59 -0500

    the file route will handle the html page that user gets sent to

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,24 @@
-var db = require("../models");
+// Dependencies
+// =============================================================
+var path = require("path");
 
+// Routes
+// =============================================================
 module.exports = function(app) {
-  // Load index page
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads index.html
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // user route loads user.html
+  app.get("/user", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/user.html"));
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // form route loads form.html
+  app.get("/form", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/form.html"));
   });
 };
